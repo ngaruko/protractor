5bdd84a9eef943016ff7b128c99129fd838d63b6
ngaruko <ngarukob@yahoo.com>
2017-03-19 21:08:17 +1300

    deleted useless stuff

--- pages/cart/cart.po.js
+++
@@ -1,9 +0,0 @@
-var CartPage = function() {
-
-    //xpath
-    this.errorMessageEmptyCart = element(by.xpath(".//*[@id='newempty']/div/h2"));
-
-    this.emptyCartNotificationText = 'Your cart is empty.';
-}
-
-module.exports = CartPage;
